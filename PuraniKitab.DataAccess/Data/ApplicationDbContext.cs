6ffc2efb2440fb295c02e96d6d6193198961f2df
Mayank Sharma <mayanksharmaggn007@rediffmail.com>
2020-10-08 14:52:24 +0530

    static landing page and layout change

--- PuraniKitab.DataAccess/Data/ApplicationDbContext.cs
+++ PuraniKitab.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PuraniKitab.Models;
 
 namespace PuraniKitab.DataAccess.Data
 {
@@ -12,5 +13,6 @@ namespace PuraniKitab.DataAccess.Data
             : base(options)
         {
         }
+         public DbSet<CategoryModel> Categories { get; set; }
     }
 }
--- PuraniKitab.DataAccess/Migrations/20201006075617_AddCategorytoDb.Designer.cs
+++ PuraniKitab.DataAccess/Migrations/20201006075617_AddCategorytoDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PuraniKitab.DataAccess.Data;
 
 namespace PuraniKitab.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201006075617_AddCategorytoDb")]
+    partial class AddCategorytoDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
